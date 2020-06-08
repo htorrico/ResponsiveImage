8e93d4b00c89e8979bbdbffbf468ec1b2190161b
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-06-07 22:26:32 -0500

    Agregar archivos de proyecto.

---
+++ ResponsiveImage/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ResponsiveImage
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
